e6d7056726d1b04960e8de457334023f1e10012a
Washington Vieira <wv817816@gmail.com>
2024-09-23 22:17:36 -0300

    verificando com um isinstance

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # Exemplo typeError
 
-try:
-    resultado = len("washington")
-    print(resultado)
-except TypeError as e:
-    print(e)
+# try:
+#     resultado = len("washington")
+#     print(resultado)
+# except TypeError as e:
+#     print(e)
+# else:
+#     print("tudo ocorreu bem")
+# finally:
+#     print("o importante e participar")
+
+numero = int(input("insira um numero: "))
+#isinstance consigo verificar a variavél se é do tipo que preciso
+if isinstance(numero, int):
+    print("A variavel é um inteiro")
 else:
-    print("tudo ocorreu bem")
-finally:
-    print("o importante e participar")
+    print("A variavel não é um inteiro")
