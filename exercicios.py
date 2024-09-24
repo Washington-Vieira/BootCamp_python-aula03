4a4fdcd98567713c2dc5bb45c5cb0f27358c2167
Washington Vieira <wv817816@gmail.com>
2024-09-23 21:26:23 -0300

    adicionei uma data split

--- exercicios.py
+++ exercicios.py
@@ -8,7 +8,14 @@ import math
 # print(resultado)
 
 
-raio_do_circulo = float(input("Digite o raio: "))
-area_do_circulo =  math.pi * raio_do_circulo ** 2
-#area_do_circulo_formatada = "{:.2f}".format(area_do_circulo)
-print(f"{area_do_circulo:.2f}")
+# raio_do_circulo = float(input("Digite o raio: "))
+# area_do_circulo =  math.pi * raio_do_circulo ** 2
+# #area_do_circulo_formatada = "{:.2f}".format(area_do_circulo)
+# print(f"{area_do_circulo:.2f}")
+
+input("insira uma data no formato dd/mm/aaaa")
+data_do_usuario = "23/09/2024"
+lista_de_dia_mes_ano = data_do_usuario.split("/")
+print(f"O elemento 1 e o : {lista_de_dia_mes_ano[0]}")
+print(f"O elemento 2 e o : {lista_de_dia_mes_ano[1]}")
+print(f"O elemento 3 e o : {lista_de_dia_mes_ano[2]}")
